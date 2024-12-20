aac9ea3769cedb5c3391111c8e8ac9ebef29418a
Busra Gazel <busragzl96@gmail.com>
2024-12-21 01:35:58 +0300

    Timer ile geri sayım yapıldı.

--- TimerProject/ViewController.swift
+++ TimerProject/ViewController.swift
@@ -2,18 +2,45 @@
 //  ViewController.swift
 //  TimerProject
 //
-//  Created by Musa on 21.12.2024.
+//  Created by Büşra Gazel on 21.12.2024.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    var timer = Timer()
+    var counter = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        counter = 10
+        timeLabel.text = "Time : \(counter)"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunc), userInfo: nil, repeats: true)
+           
+        
+    }
+    
+    @objc func timerFunc() {
+        
+        timeLabel.text = "Time : \(counter)"
+        counter -= 1
+        
+        if counter == 0 {
+            timer.invalidate() // timer durduruluyor.
+            timeLabel.text = "Time's Over"
+        }
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+        print("Button clicked")
+        
+    }
+    
 }
 
